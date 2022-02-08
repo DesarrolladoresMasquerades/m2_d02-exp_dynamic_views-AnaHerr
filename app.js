5f55413655ce50d631856ba452f7408fc9ed0267
AnaHerr <ana.herrera.91@gmail.com>
2022-02-08 17:20:15 +0100

    1st commit exercise

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+const anaData = require("./ana.json");
 const express = require("express");
 
 require("dotenv").config();
 
 const app = express();
 
+app.set("view engine", "hbs")
+
+app.set("views", __dirname + "/views")
+
 app.use(express.static("public"));
 
 
 app.get("/home", (req, res, next) =>
-res.sendFile(__dirname + "/views/home.html")
+res.render("home")
 );
 
 app.get("/about", (req, res, next) =>
-res.sendFile(__dirname + "/views/about.html")
+res.render("about")
 );
 
-app.get("/", (req, res, next) => res.send("Hello there!"));
+app.get("/", (req, res, next) => 
+ //if you need specific format for the data prepare it outse of the render and then render it
+res.render("index", anaData));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("🏃‍ on port 3000"));
